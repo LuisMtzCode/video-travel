3649484edffbd93b6186989bcb8b4bf128947906
Luis Martinez <luis.mtz.code@gmail.com>
2023-12-16 11:28:39 -0600

    Fix URLs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,20 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+let site = undefined;
+let base = "";
+
+const isProduction = import.meta.env.PROD;
+
+if (isProduction) {
+  // Production environment logic
+  site = "https://LuisMtzCode.github.io";
+  base = "/video-travel";
+}
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://LuisMtzCode.github.io",
-  base: "/video-travel",
+  site,
+  base,
   integrations: [tailwind()],
 });
